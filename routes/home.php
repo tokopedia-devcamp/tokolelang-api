7270cf5765ff150c6348d1ca1928ec89ebe94346
fannyhasbi <fnyhsbi@gmail.com>
2018-08-31 02:33:13 +0700

    Initial homepage design

--- routes/home.php
+++ routes/home.php
@@ -1,6 +1,5 @@
 <?php
-require_once "./controllers/home.php";
 
-$home = new Home();
-
-Flight::route('GET /', [$home, 'getHome']);
+Flight::route('GET /', function(){
+  Flight::render('home.php');
+});
---
+++ views/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TOKOLELANG</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+<h1>Tokolelang</h1>
+
+</body>
+</html>
